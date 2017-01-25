d374140eee0682ff14495e37ec0e0f9d08a51a36
Mantas <M.Serapinas@gmail.com>
2017-01-25 14:46:20 +0000

    Refactored and cleaned LeaderboardAdapter class

--- app/src/main/java/com/grabble/android/mantas/LeaderboardAdapter.java
+++ app/src/main/java/com/grabble/android/mantas/LeaderboardAdapter.java
@@ -10,60 +10,79 @@ import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.TextView;
 
+import org.w3c.dom.Text;
+
 /**
  * Created by Mantas on 14/01/2017.
  */
 
 public class LeaderboardAdapter extends ArrayAdapter<User> {
 
+    private LayoutInflater inflater;
+
+    private SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(getContext());
+    private String currentUserNickname = sharedPrefs.getString(
+            getContext().getString(R.string.pref_user_nickname_key),
+            getContext().getString(R.string.pref_user_nickname_default));
 
     public LeaderboardAdapter(Context context, int resource) {
         super(context, resource);
+        this.inflater = LayoutInflater.from(context);
     }
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
+        UserViewHolder holder;
 
-        View v = convertView;
+        if (convertView == null) {
+            convertView = inflater.inflate(R.layout.list_item_leaderboard, parent, false);
+            holder = new UserViewHolder();
+            holder.place = (TextView) convertView.findViewById(R.id.place);
+            holder.nickname = (TextView) convertView.findViewById(R.id.nickname);
+            holder.score = (TextView) convertView.findViewById(R.id.score);
 
-        if (v == null) {
-            LayoutInflater layoutInflater = LayoutInflater.from(getContext());
-            v = layoutInflater.inflate(R.layout.list_item_leaderboard, null);
+            convertView.setTag(holder);
+        } else {
+            holder = (UserViewHolder) convertView.getTag();
         }
 
         User user = getItem(position);
 
         if (user != null) {
-            SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(getContext());
-            String currentUserNickname = sharedPrefs.getString(
-                    getContext().getString(R.string.pref_user_nickname_key),
-                    getContext().getString(R.string.pref_user_nickname_default));
+            holder.place.setText(String.format("%d", user.getPlace()));
+            holder.nickname.setText(user.getNickname());
+            holder.score.setText(String.format("%d", user.getScore()));
 
-            TextView place = (TextView) v.findViewById(R.id.place);
-            TextView nickname = (TextView) v.findViewById(R.id.nickname);
-            TextView score = (TextView) v.findViewById(R.id.score);
-
-            if (place != null) {
-                place.setText(user.getPlace().toString());
-            }
-            if (nickname != null) {
-                nickname.setText(user.getNickname());
-            }
-            if (score != null) {
-                score.setText(user.getScore().toString());
-            }
-
-            if (currentUserNickname.equals(user.getNickname())) {
-                place.setTextColor(Color.parseColor(getContext().getString(R.string.color_light_blue)));
-                nickname.setTextColor(Color.parseColor(getContext().getString(R.string.color_light_blue)));
-                score.setTextColor(Color.parseColor(getContext().getString(R.string.color_light_blue)));
+            // Highlight the current user
+            if (isCurrentUser(user.getNickname())) {
+                highlightUser(holder);
             } else {
-                place.setTextColor(Color.WHITE);
-                nickname.setTextColor(Color.WHITE);
-                score.setTextColor(Color.WHITE);
+                holder.place.setTextColor(Color.WHITE);
+                holder.nickname.setTextColor(Color.WHITE);
+                holder.score.setTextColor(Color.WHITE);
             }
         }
 
-        return v;
+        return convertView;
+    }
+
+    /**
+     * Using ViewHolder for performance reasons.
+     * More info: https://dl.google.com/googleio/2010/android-world-of-listview-android.pdf
+     */
+    private static class UserViewHolder {
+        TextView place;
+        TextView nickname;
+        TextView score;
+    }
+
+    private boolean isCurrentUser(String nickname) {
+        return currentUserNickname.equals(nickname);
+    }
+
+    private void highlightUser(UserViewHolder holder) {
+        holder.place.setTextColor(Color.parseColor(getContext().getString(R.string.color_light_blue)));
+        holder.nickname.setTextColor(Color.parseColor(getContext().getString(R.string.color_light_blue)));
+        holder.score.setTextColor(Color.parseColor(getContext().getString(R.string.color_light_blue)));
     }
 }
